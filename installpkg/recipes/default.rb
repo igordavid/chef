d6dfdaea19ce75b2b4706fb78b79dc836897276a
Igor David <igord@bra.in.rs>
2014-07-17 16:41:38 +0000

    added munin and postfix services

--- installpkg/recipes/default.rb
+++ installpkg/recipes/default.rb
@@ -1,3 +1,23 @@
 package 'postfix' do
 	action :install
 end
+
+
+package 'munin' do
+	action :install
+end
+
+service 'postfix' do:
+	action: start
+end
+
+service 'sendmail' do
+	action :stop
+end
+
+template "/etc/munin/munin-node.conf" do
+	source "munin-node.conf.erb"
+	mode 0644
+	only_if {node['createfile']['install_file']]
+	notifies :restart, resources(:service => 'munin-node')
+end
