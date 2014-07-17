5cd505f1b5738296ad88a224a5bdcc5d842be6a7
Igor David <igord@bra.in.rs>
2014-07-17 17:30:19 +0000

    ispravljena greska u sintaksi u default.rs

--- installpkg/recipes/default.rb
+++ installpkg/recipes/default.rb
@@ -15,13 +15,20 @@ service 'sendmail' do
 	action :stop
 end
 
-service 'munin-node' do
-	action :enable
+
+#template "/etc/munin/munin-node.conf" do
+#	source "munin-node.conf.erb"
+#	mode 0644
+#	only_if {node['createfile']['install_file']}
+#	notifies :restart, resources(:service => 'munin-node')
+#end
+#
+cookbook_file "/etc/munin/munin-node.conf" do
+	source "munin-node.conf"
+	mode 0644
+	action :create_if_missing
 end
 
-template "/etc/munin/munin-node.conf" do
-	source "munin-node.conf.erb"
-	mode 0644
-	only_if {node['createfile']['install_file']}
-	notifies :restart, resources(:service => 'munin-node')
+service 'munin-node' do
+        action :start
 end
