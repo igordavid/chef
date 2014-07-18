169efd73d594f3721fa31cd9e6b6fb31254cf394
Igor David <igord@bra.in.rs>
2014-07-18 13:37:33 +0000

    dodata customizacija za apache2

---
+++ apache2/attributes/customize.rb
@@ -0,0 +1,20 @@
+###
+## This is the place to override the apache2 cookbook's default attributes.
+##
+## The following shows how to override the Apache contact and timeout settings:
+##
+
+normal[:apache][:contact] = 'igord@bra.in.rs'
+##normal[:apache][:timeout] = 60
+#
+
+#Prefork settings
+default[:apache][:prefork][:startservers] = 4
+default[:apache][:prefork][:minspareservers] = 8
+default[:apache][:prefork][:maxspareservers] = 10
+default[:apache][:prefork][:serverlimit] = 48
+default[:apache][:prefork][:maxclients] = 128
+default[:apache][:prefork][:maxrequestworkers] = 100
+default[:apache][:prefork][:maxrequestsperchild] = 1000
+default[:apache][:prefork][:maxconnectionsperchild] = 1000
+
