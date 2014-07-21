d828a331d2ba3deb48d0cb888f2a9e7a55efda4d
Igor David <igord@bra.in.rs>
2014-07-21 18:24:47 +0000

    dodata skripta za instalaciju cloudwatch logs

---
+++ cloudwatchlogs/recipes/config.rb
@@ -0,0 +1,10 @@
+# install cloudwatch logs
+#
+
+template "/tmp/cwlogs.cfg" do
+  cookbook "logs"
+  source "cwlogs.cfg.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
---
+++ cloudwatchlogs/recipes/install.rb
@@ -0,0 +1,15 @@
+# install and starts cloudwatch logs agent on the instance:
+directory "/opt/aws/cloudwatch" do
+ recursive true
+end
+
+
+remote_file "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py" do
+source "https://s3.amazonaws.com/aws-cloudwatch/downloads/awslogs-agent-setup-v1.0.py"
+mode "0755"
+end
+
+execute "Install CloudWatch Logs agent" do
+command "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py -n -r us-east-1 -c /tmp/cwlogs.cfg"
+not_if { system "pgrep -f aws-logs-agent-setup" }
+end
