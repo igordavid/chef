97e7a893909f6b1f6bd401be5a8413669e7bd18c
Igor David <igord@bra.in.rs>
2014-07-21 16:56:09 +0000

    dodata skripta za reboot munina i kastomizacija apache2 servera

--- apache2/recipes/setup-logs.rb
+++ apache2/recipes/setup-logs.rb
@@ -5,5 +5,5 @@ end
 cookbook_file "/etc/httpd/sites-available/igordnet.conf.d/local-igord.net.conf" do
         source "local-igord.net.conf"
         mode 0644
-        notifies :restart, resources(:services => 'httpd')
+        notifies :restart, resources(:service => 'httpd')
 end
