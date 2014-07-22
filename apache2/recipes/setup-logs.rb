c5b86ae01dab6488e30fbfe9912af79e09d22caf
Igor David <igord@bra.in.rs>
2014-07-22 17:38:15 +0000

    dodat drugi konfiguracioni fajl za apache za cloudhowto.org

--- apache2/recipes/setup-logs.rb
+++ apache2/recipes/setup-logs.rb
@@ -10,7 +10,7 @@ end
 
 
 
-cookbook_file "/etc/httpd/sites-available/igordnet.conf.d/local-cloudhowto.org.conf" do
+cookbook_file "/etc/httpd/sites-available/cloudhowtoorg.conf.d/local-cloudhowto.org.conf" do
         source "local-cloudhowto.org.conf"
         mode 0644
         notifies :restart, resources(:service => 'httpd')
