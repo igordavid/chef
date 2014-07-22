dd65d23c92d660befe5c6b13834baf9a57033ed2
Igor David <igord@bra.in.rs>
2014-07-22 17:28:12 +0000

    dodata skripta za instalaciju cloudwatch logs

--- apache2/recipes/setup-logs.rb
+++ apache2/recipes/setup-logs.rb
@@ -7,3 +7,11 @@ cookbook_file "/etc/httpd/sites-available/igordnet.conf.d/local-igord.net.conf" 
         mode 0644
         notifies :restart, resources(:service => 'httpd')
 end
+
+
+
+cookbook_file "/etc/httpd/sites-available/igordnet.conf.d/local-cloudhowto.org.conf" do
+        source "local-cloudhowto.org.conf"
+        mode 0644
+        notifies :restart, resources(:service => 'httpd')
+end
