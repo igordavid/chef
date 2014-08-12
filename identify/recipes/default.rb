ccdddd8398ded77587333c0702d429e31d481bf5
Igor David <igord@bra.in.rs>
2014-08-12 13:39:17 +0000

    changed identify

--- identify/recipes/default.rb
+++ identify/recipes/default.rb
@@ -1,5 +1,6 @@
 ruby_block "identify" do
 	block do
+		HOST = #{node['hostname']}
 		require 'net/smtp'
 		message = <<MESSAGE_END
 		From: NODE <#{node['hostname']}>
@@ -15,7 +16,7 @@ ruby_block "identify" do
 MESSAGE_END
 
 		Net::SMTP.start('localhost') do |smtp|
-		smtp.send_message message, 	'root@#{node[\'hostname\']}',
+		smtp.send_message message, 	'root@#{HOST}',
 						'igord@bra.in.rs'
 		end
 	end
