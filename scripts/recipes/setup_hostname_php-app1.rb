544f7185cd25892079a5ef5cd6d93a479d20e2f4
Igor David <igord@bra.in.rs>
2014-09-16 15:11:55 +0000

    adding hostname to opsworks template opsworks_stack_state_sync

--- scripts/recipes/setup_hostname_php-app1.rb
+++
@@ -1,7 +0,0 @@
-bash "setup_hostname_php-app1" do
-	user "root"
-	code <<-EOH
-		echo "php-app1.igord.net" > /proc/sys/kernel/hostname
-		/etc/init.d/postfix restart
-	EOH
-end
--- scripts/recipes/setup_hostname_php-app2.rb
+++
@@ -1,7 +0,0 @@
-bash "setup_hostname_php-app2" do
-	user "root"
-	code <<-EOH
-		echo "php-app2.igord.net" > /proc/sys/kernel/hostname
-		/etc/init.d/postfix restart
-	EOH
-end
